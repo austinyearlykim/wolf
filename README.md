cc7b4475c82d7caefc20bac774845c41da493fc1
Austin Yearly Kim <austinyearlykim@gmail.com>
2018-01-28 23:24:25 -0800

    Refactor v2.0.0 (#6)
    v2.0.0

--- README.md
+++ README.md
@@ -1,55 +1,46 @@
 # Project W.O.L.F
     Binance trading bot for NodeJS.
 
-### What it looks like
-![alt text](https://imgur.com/fNKvAOy.png "Proof")
-
-### Proof
-![alt text](https://imgur.com/pqjqGrd.png "Proof")
-
 IT IS NOT MY RESPONSIBILITY IF YOU GAIN/LOSE MONEY.  THERE IS NO SUCH THING AS PERFECT SOFTWARE.  PLEASE LOOK THROUGH THE CODEBASE/DOCUMENTATION TO UNDERSTAND THE RISKS YOU ARE TAKING.  
 
 ### Setup
-1. Create an .env file in your root directory.
-2. Copy and paste template.env into .env
-3. Fill out required environment variables
+1. Create an `.env` file in your root directory.
+2. Copy and paste `template.env` into `.env`
+3. Fill out *required* environment variables
 ---> Sign up for Binance API here if you haven't already: https://www.binance.com/userCenter/createApi.html
 4. `npm i`
 5. `npm start`
 
 ### Documentation
 ##### `How it works`
-W.O.L.F calculates how much to spend based on your budget.  It watches price movements of a particular `TRADING_PAIR` in real-time.  It can only ever have one buy/sell transaction happen at once, therefore you can only lose as much as you budget. And even then it's not really considered a loss because you are still holding the asset W.O.L.F purchased for you.  You can sell that asset yourself at any time.
+W.O.L.F calculates how much to spend *per transaction* based on your `BUDGET`.  It watches price movements of a particular `TRADING_PAIR` in real-time and will buy at the current price and sell for a calculated `PROFIT_PERCENTAGE`.
 
-The synchronous nature of the chain of functions that W.O.L.F executes with the real-time data it watches makes sure that your trades execute as fast AND safe as possible.  And only when it's profitable!  Transaction fees are taken account of!  Easy peasy!
+The synchronous nature of W.O.L.F makes sure that your trades execute as fast *and* safe as possible.  And only sells when it's profitable!  Transaction fees are taken account of!  Easy peasy!
+
+Brief technical explanation:  There are three parts to W.O.L.F.  There is a `ticker`, `queue`, and `consumer`.  The `ticker` keeps track of current prices in real-time and acts a heartbeat. The `ticker` will try to execute a purchase order every tick if it's not already executing one, as well as firing off the `consumer` if it's not already consuming.  Executed trades get put into a `queue` that holds open, or unfilled, orders in memory.  The `consumer` is responsible for checking if these open orders have been filled, and if so remove them from the queue.  The `consumer` will then add back in the queue the open and unfilled, closing order.  Rinse and repeat.
 
 ##### `.env`
-- Currently the only thoroughly tested `TRADING_PAIR` is `ETHBTC`.
-- If `TRADING_PAIR` equals `ETHBTC` it means you're buying and selling Ethereum with Bitcoin.
+- `BUDGET` is the most you're willing to spend.  The unit of this number is the second half of `TRADING_PAIR`; e.g if `TRADING_PAIR`is `ETHBTC` then `BUDGET`is the amount of BTC you're willing to spend.
 - `PROFIT_PERCENTAGE` is in whole numbers; e.g `1.2` is one-point-two percent.
-- `BUDGET` is the most you're willing to lose.  The unit of this number is the second half of `TRADING_PAIR`; e.g if `TRADING_PAIR`is `ETHBTC` then `BUDGET`is the amount of BTC you're willing to lose.
+- `TRADING_PAIR` must be in upper-case; e.g if `TRADING_PAIR` equals `ETHBTC` it means you're buying and selling Ethereum with Bitcoin.
 
 ##### `npm start`
-This command runs tests before starting the bot.  It then kicks off a recusive loop of functions that keep track of best BUY/SELL prices updated by the second and executes trades that are favored for you.
+This command runs tests before starting the bot.  It then kicks off a recursive loop of functions that keep track of best BUY/SELL prices updated by the second and executes trades that are favored for you.
 
 ##### `Ctrl + C`
-Pressing these two keys will kill W.O.L.F.  Pay particular attention to where in the function loop W.O.L.F was killed.  You may need to login to https://www.binance.com/userCenter/openOrders.html to kill off any open orders that W.O.L.F is no longer watching for you; but again, that's only if you didn't kill W.O.L.F right after a `[SOLD]::::` console.log().
+Pressing these two keys will terminate W.O.L.F.  Pay particular attention to where in the function loop W.O.L.F was terminated.  You may need to login to https://www.binance.com/userCenter/openOrders.html to close any open orders that W.O.L.F is no longer watching for you.
 
-Future features will alleviate this user-error prone exit strategy.
-
-##### `[PURCHASING]::::`
+### Logs
+##### `Purchasing... `
 Limit Buy Order was PLACED.
 
-##### `[PURCHASED]::::`
+##### `Purchased. `
 Limit Buy Order was FILLED.
 
-##### `[HODL]::::`
-You now have a position(the coins W.O.L.F purchased).  W.O.L.F is waiting for an optimal price to sell it at to "pseudo-guarantee" execution of a limit sell order.
-
-##### `[SELLING]::::`
+##### `Selling... `
 Limit Sell Order was PLACED.
 
-##### `[SOLD]::::`
+##### `Sold. `
 Limit Sell Order was FILLED.
 
 ### Issues?
---
+++ modules/wolf.js
@@ -0,0 +1,146 @@
+const binance = require('./binance.js');
+const twilio = require('./twilio.js');
+const fs = require('fs');
+const assert = require('assert');
+
+module.exports = class Wolf {
+    constructor(config) {
+        this.config = config;
+        this.symbol = null;
+        this.ticker = null;  //binance websocket responsible for updating bid/ask prices per second
+        this.tick = null;  //up to date bid/ask prices {bid: 0.12345678, ask: 0.12345678}
+        this.executing = false;
+        this.consuming = false;
+        this.queue = [];
+        this.init();
+    }
+
+    //initiate ticker
+    async init() {
+        //get trading pair information
+        const exchangeInfo = await binance.exchangeInfo();
+        exchangeInfo.symbols.forEach((symbol) => {
+            if (symbol.symbol === this.config.tradingPair) return this.symbol = symbol;
+        });
+        //setup/start ticker
+        this.ticker = binance.ws.partialDepth({ symbol: this.config.tradingPair, level: 5 }, (depth) => {
+            const temp = {};
+            temp.bid = Number(depth.bids[0].price);
+            temp.ask = Number(depth.asks[0].price);
+            this.tick = temp;
+            !this.executing && this.execute();
+            !this.consuming && this.consume();
+        });
+    }
+
+    //execute W.O.L.F
+    execute() {
+        this.executing = true;
+        this.logger('Executing...', '');
+        this.purchase();
+    }
+
+    //digest the queue of open buy/sell orders
+    async consume() {
+        if (!this.queue.length) return;
+        this.consuming = true;
+        this.logger('Consuming queue...', 'Orders in queue: ' + this.queue.length);
+
+        //iterate through queue and hold in memory FILLED transactions
+        const filledTransactions = {};
+        for (let i = 0; i < this.queue.length; i++) {
+            const txn = this.queue[i];
+            const transaction = await binance.getOrder({ symbol: this.config.tradingPair, orderId: txn.orderId });
+            if (transaction.status === 'FILLED') {
+                filledTransactions[txn.orderId] = transaction;
+                const side = transaction.side === 'BUY' ? 'PURCHASED' : 'SOLD';
+                this.logger(side + ': ' + transaction.executedQty + transaction.symbol + ' @ ', transaction.price);
+                this.writeToLedger(Date.now(), transaction.symbol, transaction.side, transaction.executedQty, transaction.price);
+                if (transaction.side === 'SELL') {
+                    await twilio.sendText(`${side} ${transaction.symbol}`);
+                    this.executing = false;
+                }
+            }
+        }
+        const orderIds = Object.keys(filledTransactions);
+
+        //filter out all FILLED filledTransactions from queue
+        this.queue = this.queue.filter((txn) => {
+            return orderIds.indexOf(txn.orderId) === -1;
+        });
+
+        //repopulate queue with closing (unconfirmed) transactions
+        for (let key in filledTransactions) {
+            const txn = filledTransactions[key];
+            if (txn.side === 'BUY') {
+                const price = Number(txn.price);
+                const profit = price + (price * Number(this.config.profitPercentage) + (price * .001));
+                this.sell(txn.exectedQty, profit);
+            }
+            if (txn.side === 'SELL') {
+                const price = Number(txn.price);
+                const profit = price - (price * Number(this.config.profitPercentage));
+                this.purchase(txn.exectedQty, profit);
+            }
+        }
+
+        this.logger('Consumed queue.', 'Orders in queue: ' + this.queue.length);
+        this.consuming = false;
+    }
+
+    //calculate quantity of coin to purchase based on given budget from .env
+    calculateQuantity() {
+        this.logger('Calculating quantity...', '');
+        const minQuantity = Number(this.symbol.filters[1].minQty);
+        const maxQuantity = Number(this.symbol.filters[1].maxQty);
+        const stepSize = Number(this.symbol.filters[1].stepSize);  //minimum quantity difference you can trade by
+        const currentPrice = this.tick.ask;
+        const budget = Number(this.config.budget);
+
+        let quantity = minQuantity;
+        while (quantity * currentPrice <= budget) quantity += stepSize;
+        if (quantity * currentPrice > budget) quantity -= stepSize;
+        if (quantity === 0) quantity = minQuantity;
+
+        assert(quantity >= minQuantity && quantity <= maxQuantity, 'invalid quantity');
+
+        this.logger('Quantity Calculated: ', quantity.toFixed(8));
+        return quantity.toFixed(8);
+    }
+
+    //purchase quantity of coin @ this.tick.bid and only continue executing W.O.L.F if this limit buy order is FILLED.
+    async purchase(quantity, price) {
+        const tickSize = Number(this.symbol.filters[0].tickSize);  //minimum price difference you can trade by
+        const sigFig = (this.symbol.filters[0].minPrice).indexOf('1') - 2;
+        const unconfirmedPurchase = await binance.orderTest({ symbol: this.config.tradingPair, side: 'BUY', quantity: (quantity && quantity.toFixed(8)) || this.calculateQuantity(), price: (price && price.toFixed(sigFig)) || (this.tick.bid + tickSize).toFixed(sigFig) });
+        this.queue.push(unconfirmedPurchase);
+        this.logger('Purchasing...', unconfirmedPurchase.symbol);
+    }
+
+    //sell quantity of coin and only continue executing W.O.L.F if this limit sell order is FILLED.
+    async sell(quantity, profit) {
+        const tickSize = Number(this.symbol.filters[0].tickSize);  //minimum price difference you can trade by
+        const sigFig = (this.symbol.filters[0].minPrice).indexOf('1') - 2;
+        const unconfirmedSell = await binance.orderTest({ symbol: this.config.tradingPair, side: 'SELL', quantity: quantity.toFixed(8), price: profit.toFixed(sigFig) });
+        this.queue.push(unconfirmedSell);
+        this.logger('Selling...', unconfirmedSell.symbol);
+    }
+
+    //function to stop W.O.L.F and kill the node process
+    terminate() {
+        this.logger('Terminating W.O.L.F...');
+        process.exit(0);
+    }
+
+    //utility function to console.log formatted messages
+    logger(a, b) {
+        if (process.env.LOGGING === 'true' || process.env.LOGGING === 'TRUE') {
+            console.log(`[WOLF]:::: ${a} ${b}`);
+        }
+    }
+
+    //function to log profits to a ledger.csv file
+    async writeToLedger(date, pair, side, amount, price) {
+        fs.appendFileSync('ledger.csv', `${date} ${pair} ${side} ${amount} ${price} \n`);
+    }
+};
